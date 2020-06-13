c95ae35abcbd3fa708ff2051de612e3ff438c5c0
David Gonon <david.gonon@epfl.ch>
2020-06-13 02:47:42 +0200

    script for overcoming_03 plots. some new mat files

--- eva/scripts/capsule_distance.py
+++ eva/scripts/capsule_distance.py
@@ -1,4 +1,6 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Capsule:
 	def __init__(self, y_front, y_back, r):
@@ -13,3 +15,22 @@ class Capsule:
 			return math.sqrt(x*x + (y - self.y_back)*(y - self.y_back)) - self.r
 		else:
 			return math.fabs(x) - self.r
+
+	def plot_at_pose(self, x, y, phi, ax):
+		R = np.array([
+			[math.cos(phi), -math.sin(phi)],
+			[math.sin(phi),  math.cos(phi)]])
+		T = np.array([x, y])
+		c_front = np.matmul(R, np.array([0.0, self.y_front])) + T
+		c_back = np.matmul(R, np.array([0.0, self.y_back])) + T
+		left_front = np.matmul(R, np.array([-self.r, self.y_front])) + T
+		left_back = np.matmul(R, np.array([-self.r, self.y_back])) + T
+		right_front = np.matmul(R, np.array([self.r, self.y_front])) + T
+		right_back = np.matmul(R, np.array([self.r, self.y_back])) + T
+
+		circle_front = plt.Circle((c_front[0], c_front[1]), self.r, color='b', fill=False)
+		circle_back = plt.Circle((c_back[0], c_back[1]), self.r, color='b', fill=False)
+		ax.add_artist(circle_front)
+		ax.add_artist(circle_back)
+		ax.plot([left_front[0], left_back[0]], [left_front[1], left_back[1]], 'b')
+		ax.plot([right_front[0], right_back[0]], [right_front[1], right_back[1]], 'b')
