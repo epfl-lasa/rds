ed5d5f91652485b2e741e017594affe1ef71af9f
David Gonon <david.gonon@epfl.ch>
2020-06-22 00:03:30 +0200

    add metrics plot and unity results plot scripts

---
+++ eva/scripts/metrics_plot.py
@@ -0,0 +1,40 @@
+import csv
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams.update({'font.size': 22})
+
+filename = "../../rds/metrics_evaluation.csv"
+
+E_t_RDS = []
+E_t_ORCA = []
+N_ttg_RDS = []
+N_ttg_ORCA = []
+
+with open(filename) as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';')
+    i = 0
+    for row in spamreader:
+    	if i == 0:
+    		i += 1
+    		continue
+    	E_t_RDS.append(1.0/float(row[0]))
+    	E_t_ORCA.append(1.0/float(row[1]))
+    	N_ttg_RDS.append(float(row[4]))
+    	N_ttg_ORCA.append(float(row[5]))
+        i += 1
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('Crowd time to goal ratio')
+ax1.boxplot([E_t_RDS, E_t_ORCA], notch=True)
+plt.xticks([1, 2], ['RDS', 'ORCA'])
+ax1.set_ylabel("$E_t$ [-]")
+plt.savefig("boxplot_E_t.png",bbox_inches='tight',dpi=100)
+plt.show()
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('Neighbours time to goal ratio')
+ax1.boxplot([N_ttg_RDS, N_ttg_ORCA], notch=True)
+plt.xticks([1, 2], ['RDS', 'ORCA'])
+ax1.set_ylabel("$N_{ttg}$ [-]")
+plt.savefig("boxplot_N_ttg.png",bbox_inches='tight',dpi=100)
+plt.show()
---
+++ eva/scripts/unity_plot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.rcParams.update({'font.size': 16})
+
+T = np.array([ [47.1, 35.3, 28.0], [70.3, 52.9, 43.0] ])
+V = np.array([ [0.83, 1.02, 1.36], [0.56, 0.74, 0.91],
+	[0.31, 0.43, 0.51] ])
+v_nominal = np.array([0.9, 1.2, 1.5])
+density = np.array([0.2, 0.6, 1.0])
+
+
+plt.plot(v_nominal, V[0, :], "ko-", label=r"$v$ for $\rho=" + str(density[0]) + "$")
+plt.plot(v_nominal, V[1, :], "ro-", label=r"$v$ for $\rho=" + str(density[1]) + "$")
+plt.plot(v_nominal, V[2, :], "co-", label=r"$v$ for $\rho=" + str(density[2]) + "$")
+plt.gca().set_ylabel("$v$ [m/s]")
+plt.gca().set_xlabel("$v_{des}$ [m/s]")
+plt.legend()
+plt.gca().set_aspect("equal")
+v_range = [0.25, 1.55]
+plt.gca().set_xlim(v_range)
+plt.gca().set_ylim(v_range)
+plt.plot(v_range, v_range, "k:")
+plt.savefig("v_unity.png",bbox_inches='tight',dpi=100)
+plt.show()
+
+plt.plot(v_nominal, T[0, :], "ko-", label=r"$T$ for $\rho=" + str(density[0]) + "$")
+plt.plot(v_nominal, T[1, :], "ro-", label=r"$T$ for $\rho=" + str(density[1]) + "$")
+plt.gca().set_ylabel("$T$ [s]")
+plt.gca().set_xlabel("$v_{des}$ [m/s]")
+plt.legend()
+v_range = [0.85, 1.55]
+plt.gca().set_xlim(v_range)
+t_range = [25.0, 75.0]
+plt.gca().set_ylim(t_range)
+plt.savefig("T_unity.png",bbox_inches='tight',dpi=100)
+plt.show()
