518604f022881264f5e5785dd8e52afecca88abe
David Gonon <david.gonon@epfl.ch>
2020-07-23 18:13:24 +0200

    update fave plot style and add crowd overtaking

--- fave/all_plots.py
+++ fave/all_plots.py
@@ -73,6 +73,8 @@ def plot_and_export(name, t_snapshot_1, t_snapshot_2, show_big_circle, x_lim, y_
 		if (t[i] > t_d_min_2) and (i_d_min_2 == None):
 			i_d_min_2 = i
 
+	i_snapshots = [i_d_min_1, i_d_min_2]
+	t_snapshot_colors = [[0.0,1.0, 1.0], [0.0,1.0, 0.0]]
 	plot_lrf = True
 	#lrf_sampler = np.arange(0, t.shape[0]/5, 20)
 	#lrf_points_sampled = lrf[lrf_sampler, :]
@@ -83,16 +85,17 @@ def plot_and_export(name, t_snapshot_1, t_snapshot_2, show_big_circle, x_lim, y_
 		#	plt.scatter(lrf_points_sampled[k,even].flatten(), lrf_points_sampled[k, odd].flatten())
 		#plt.scatter(lrf[0, even].flatten(), lrf[0, odd].flatten())
 		if not remove_ground:
-			plt.scatter(lrf[i_d_min_1, even].flatten(), lrf[i_d_min_1, odd].flatten(), color="g")
-			plt.scatter(lrf[i_d_min_2, even].flatten(), lrf[i_d_min_2, odd].flatten(), color="g")
+			plt.scatter(lrf[i_d_min_1, even].flatten(), lrf[i_d_min_1, odd].flatten(), color=t_snapshot_colors[0])
+			plt.scatter(lrf[i_d_min_2, even].flatten(), lrf[i_d_min_2, odd].flatten(), color=t_snapshot_colors[1])
 		else:
 			r_cutoff_squared = 9.0
-			for i_now in [i_d_min_1, i_d_min_2]:
+			for i_snap in range(2):
+				i_now = i_snapshots[i_snap]
 				X_lrf_now = lrf[i_now, even].flatten() 
 				Y_lrf_now = lrf[i_now, odd].flatten()
 				R_now_squared = np.power(X_lrf_now - xy_p_ref[i_now, 0], 2) + np.power(Y_lrf_now - xy_p_ref[i_now, 1], 2)
 				i_cutoff = R_now_squared < r_cutoff_squared
-				plt.scatter(X_lrf_now[i_cutoff], Y_lrf_now[i_cutoff], color="g")
+				plt.scatter(X_lrf_now[i_cutoff], Y_lrf_now[i_cutoff], color=t_snapshot_colors[i_snap])
 
 	plt.scatter(xy_p_ref[1:i_d_min_2, 0], xy_p_ref[1:i_d_min_2, 1], c=(t[1:i_d_min_2]-t[1])/(t[i_d_min_2]-t[1]), cmap=cm.hot_r, marker='o', edgecolors='k')
 	plt.scatter(xy_p_ref[1:i_d_min_2, 0], xy_p_ref[1:i_d_min_2, 1], c=(t[1:i_d_min_2]-t[1])/(t[i_d_min_2]-t[1]), cmap=cm.hot_r, marker='o')#, edgecolors='k')
@@ -110,12 +113,12 @@ def plot_and_export(name, t_snapshot_1, t_snapshot_2, show_big_circle, x_lim, y_
 	for j in range(tracks.shape[1]/4):
 		if np.isnan(tracks[i_d_min_1, j*4]):
 			break
-		circle_d_min_1 = plt.Circle((tracks[i_d_min_1, j*4], tracks[i_d_min_1, j*4+1]), 0.3, color='g', fill=False)
+		circle_d_min_1 = plt.Circle((tracks[i_d_min_1, j*4], tracks[i_d_min_1, j*4+1]), 0.3, color=t_snapshot_colors[0], fill=False)
 		ax.add_artist(circle_d_min_1)
 	for j in range(tracks.shape[1]/4):
 		if np.isnan(tracks[i_d_min_2, j*4]):
 			break
-		circle_d_min_2 = plt.Circle((tracks[i_d_min_2, j*4], tracks[i_d_min_2, j*4+1]), 0.3, color='g', fill=False)
+		circle_d_min_2 = plt.Circle((tracks[i_d_min_2, j*4], tracks[i_d_min_2, j*4+1]), 0.3, color=t_snapshot_colors[1], fill=False)
 		ax.add_artist(circle_d_min_2)
 #v_sampler = v_sampler = np.arange(0, t.shape[0], 10)#[i_d_min_1, i_d_min_2]
 #plt.quiver(xy_p_ref[v_sampler, 0], xy_p_ref[v_sampler, 1], v_cartesian_nominal_p_ref[v_sampler, 0], v_cartesian_nominal_p_ref[v_sampler, 1], color=(0,0,1,0.5), scale=1.0, angles='xy', scale_units='xy', width=0.005)
@@ -164,10 +167,12 @@ def plot_and_export(name, t_snapshot_1, t_snapshot_2, show_big_circle, x_lim, y_
 
 
 tests_args = [
-	["jul_16_door_rds_3", 0.0, 8.5, False, [-1.5, 4.5], [-2, 2], False],
-	["jul_16_door_orca_2", 0.0, 8.5, True, [-1.5, 2.5], [-2, 2], False],
-	["jul_17_row_overtaking_rds_o1", 2.0, 12.5, False, [0, 9.5], [-2, 3], True],
-	["jul_17_row_overtaking_orca", 2.0, 9, True, [5, 11], [-4, 1], True]
+	["jul_16_door_rds_3", 0.0, 8.5, False, [-1.5, 4.5], [-2, 2], False]
+	,["jul_16_door_orca_2", 0.0, 8.5, True, [-1.5, 2.5], [-2, 2], False]
+	,["jul_17_row_overtaking_rds_o1", 2.0, 12.5, False, [0, 9.5], [-2, 3], True]
+	,["jul_17_row_overtaking_orca", 2.0, 9, True, [5, 11], [-4, 1], True]
+	,["jul_17_crowd_overtaking_rds", 6.0, 11, False, [11.5,19], [-8.2,-1], True]
+	,["jul_17_crowd_overtaking_orca", 8.0, 17.7, True, [13.7, 19.7], [-8.5, -2.5], True]
 ]
 
 for args in tests_args:
