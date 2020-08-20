57d2f0b68ee86a7508ca08ffbdc1f8adadbf9623
David Gonon <david.gonon@epfl.ch>
2020-08-20 20:07:44 +0200

    new boxplots using slightly different data

--- rds/script/make_box_plots.py
+++ rds/script/make_box_plots.py
@@ -1,6 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# plt.rcParams.update({
+#     "text.usetex": True,
+#     "font.family": "serif",
+#     "font.serif": ["Palatino"],
+# })
+from matplotlib import rc
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size' : 14})
+rc('text', usetex=True)
+from matplotlib.ticker import FormatStrFormatter
+
 samples_metrics = np.genfromtxt('../metrics_evaluation.csv',
 	delimiter=';')
 file = open('../metrics_evaluation.csv')
@@ -21,7 +31,9 @@ for k in range(selection.shape[0]/2):
 	ax.boxplot(data, 1, 'x', showmeans=False)#, positions=[0.5,1.5])
 	#ax.set_xlim([0.0, 2.0])
 	#plt.subplots_adjust(left=0.3, right=0.7)
-	ax.set_xticklabels(labels, rotation=45, fontsize=12)
+	ax.set_xticklabels(labels, rotation=45)#, fontsize=12)
+	#ax.yaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+	plt.locator_params(axis='y', nbins=4)
 	ax.set_title(title)
 
 	plt.savefig(title[1:-1].strip()+'boxplots.png', bbox_inches='tight', dpi=199)
