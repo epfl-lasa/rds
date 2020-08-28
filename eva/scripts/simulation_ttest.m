d67f5d501b72ba47fd7cb705a0ae8245534c1176
David Gonon <david.gonon@epfl.ch>
2020-08-28 18:30:42 +0200

    update ttest m file

--- eva/scripts/simulation_ttest.m
+++ eva/scripts/simulation_ttest.m
@@ -10,7 +10,29 @@
 %% 
 clear all; close all; clc; 
 
-load('simulation_data.mat')
+%res = dlmread(filename,';')
+filename = "../../rds/metrics_evaluation.csv";
+M = dlmread(filename, ';', 1, 0);
+fid = fopen(filename);
+C = textscan(fid, '%[^\n]', 1);
+fclose(fid);
+listC = strsplit(C{1}{1}, ";");
+simulation_metrics = struct();
+for i = 1:length(listC)
+    simulation_metrics.(strtrim(listC{i})) = M(:,i);
+end
+
+h_rob_track_err = ttest2(simulation_metrics.ORCA_rob_track_err, simulation_metrics.RDS_rob_track_err)
+
+h_ped_track_err = ttest2(simulation_metrics.ORCA_ped_track_err, simulation_metrics.RDS_ped_track_err)
+
+h_E_v = ttest2(simulation_metrics.ORCA_E_v, simulation_metrics.RDS_E_v)
+
+h_N_v = ttest2(simulation_metrics.ORCA_N_v, simulation_metrics.RDS_N_v)
+
+return;
+%load('simulation_data.mat')
+
 h_E_rob_track_err = ttest(simulation_metrics.ORCA_rob_track_err, simulation_metrics.RDS_rob_track_err)
 
 h_E_t = ttest(simulation_metrics.ORCA_E_t, simulation_metrics.RDS_E_t)
